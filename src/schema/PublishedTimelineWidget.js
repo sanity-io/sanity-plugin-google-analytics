9379b721c67ad27b4732ff2334f15b1412e77cf4
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-11-11 10:18:17 +0100

    Remove form builder widget

--- src/schema/PublishedTimelineWidget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PublishedTimelineFormBuilderWidget from '../components/PublishedTimelineFormBuilderWidget'
-
-export default {
-  name: '__PublishedTimelineGAWidget__',
-  title: 'Google Analytics Widget',
-  type: 'string',
-  inputComponent: PublishedTimelineFormBuilderWidget,
-}
--- src/schema/WidgetType.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FormBuilderWidget from '../components/FormBuilderWidget'
-
-export default {
-  name: 'GAWidget',
-  title: 'Google Analytics Widget',
-  type: 'string',
-  inputComponent: FormBuilderWidget,
-}
